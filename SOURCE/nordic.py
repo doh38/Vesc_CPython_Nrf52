5fda434301558f36daa28004970e07d7c442e3d8
doh38 <42808575+doh38@users.noreply.github.com>
2023-01-18 18:10:59 +0100

    basic copyright

--- SOURCE/nordic.py
+++ SOURCE/nordic.py
@@ -2,7 +2,16 @@
 # SPDX-FileCopyrightText: 2019 Scott Shawcroft for Adafruit Industries
 #
 # SPDX-License-Identifier: MIT
-
+# Copyright 2023 Hadrien Dorfman - dodjob@yahoo.fr
+# This is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This code is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# http://www.gnu.org/licenses.
 """
 `nordic`
 ====================================================
